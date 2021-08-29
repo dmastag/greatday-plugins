ed081ba28f97a3f753a6c8c1ab44f2aa27334090
Babay <senjuid@gmail.com>
2021-08-29 22:10:08 +0700

    Create GreatDayPlugin.h

---
+++ src/ios/GreatDayPlugin.h
@@ -0,0 +1,14 @@
+#import <Cordova/CDV.h>
+
+@interface GreatDayPlugin : CDVPlugin
+
+// Public static method
++ (GreatDayPlugin*) greatDayPlugin;
+
+// A public instance method
+- (void)logMessage: (NSString*)msg;
+- (NSString *) getMessage;
+- (void)addBlurredSnapshot:(CDVInvokedUrlCommand*)command;
+- (void)removeBlurredSnapshot:(CDVInvokedUrlCommand*)command;
+
+@end
